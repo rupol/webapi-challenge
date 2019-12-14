07968b1f29c7752020d096ec7d88c59e5166e722
Ruth Poliakon <r.poliakon@gmail.com>
2019-12-13 17:35:45 -0700

    action and project routers set up and imported, logger middleware functional

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
-/*
-play this: https://www.youtube.com/watch?v=d-diB65scQU
-
-Sing along:
-
-here's a little code I wrote, please read the README word for word, don't worry, you got this
-in every task there may be trouble, but if you worry you make it double, don't worry, you got this
-ain't got no sense of what is REST? just concentrate on learning Express, don't worry, you got this
-your file is getting way too big, bring a Router and make it thin, don't worry, be crafty
-there is no data on that route, just write some code, you'll sort it out… don't worry, just API…
-I need this code, just don't know where, perhaps should make some middleware, don't worry, just API
-
-Go code!
-*/
-
 const express = require("express");
 
 const server = require("./server");
 
+const actionRouter = require("./routers/action");
+const projectRouter = require("./routers/project");
+
 server.use(express.json());
+
+server.use("/api/actions", actionRouter);
+server.use("/api/projects", projectRouter);
 
 const host = "localhost";
 const port = 4000;
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const logger = require("./middleware/logger");
 
 const server = express();
+
+server.use(logger());
 
 server.get("/", (req, res) => {
   res.send(`
