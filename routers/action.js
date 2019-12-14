d4c1a8880a66f46b33ae5a421e5801f6f0f3de3e
Ruth Poliakon <r.poliakon@gmail.com>
2019-12-13 18:31:38 -0700

    delete action and update action endpoints

--- routers/action.js
+++ routers/action.js
@@ -18,4 +18,28 @@ router.get("/:id", validateActionId(), (req, res) => {
   res.json(req.action);
 });
 
+router.delete("/:id", validateActionId(), (req, res, next) => {
+  actions
+    .remove(req.action.id)
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json({ message: "The post has been deleted" });
+      }
+    })
+    .catch(error => {
+      next(error);
+    });
+});
+
+router.put("/:id", validateActionId(), validateAction(), (req, res, next) => {
+  actions
+    .update(req.action.id, req.body)
+    .then(actionObj => {
+      res.status(200).json(actionObj);
+    })
+    .catch(error => {
+      next(error);
+    });
+});
+
 module.exports = router;
